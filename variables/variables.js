43203f8d4a2320c5ab986a2f5f342eed6498703c
Shaik Affan Ahemad <shaikaffanahemad@gmail.com>
2026-04-14 21:36:27 +0530

    Add files via upload

---
+++ variables/variables.js
@@ -0,0 +1,13 @@
+// var a = 123;
+// var a = 1478932;
+// console.log(a); // 1278932 it means in var we can make same variable with same name(which is wrong Practice)
+
+// let name = "rahul";
+// // let name = "rohil"//we can't make variable with same name in let
+// name = "romania"; //in let we can overwrite the value
+// console.log(name);
+
+// const pi = 3.14;
+// // const pi = 4.14;//we can't make variable with same name in const
+// pi=3.141;//in const we cannot overwrite the value
+// colsole.log(pi);
