c5c2c4593ba92bb2079ba7ea18160d49b8733cc9
Shaik Affan Ahemad <shaikaffanahemad@gmail.com>
2026-05-03 16:06:09 +0530

    Add files via upload

---
+++ Loops in Js/for(in)loop/for.js
@@ -0,0 +1,6 @@
+//FOR IN LOOP PROVIDE INDEXES
+let fruits = ["orange", "watermelon", "papaya", "mango"];
+for (let x in fruits) {
+  console.log(x);
+  console.log(fruits[x]);
+}
---
+++ Loops in Js/for(of)loop/my.js
@@ -0,0 +1,5 @@
+//FOR OF LOOP ELEMENT OF ARRAY
+let fruits = ["mango", "papaya", "watermelon", "grapes"];
+for (let x of fruits) {
+  console.log(x);
+}
