5ea973cf81b1857db95da3113c4bc4bf5473f80d
Shaik Affan Ahemad <shaikaffanahemad@gmail.com>
2026-05-10 16:29:03 +0530

    Add files via upload

---
+++ array basics/array.js
@@ -0,0 +1,12 @@
+// let arr1 = [2, 3, 3, 89, 90];
+// console.log(arr1);
+
+// let arr2 = ["non", "mon", "son", "array"];
+// console.log(arr2);
+
+// let arr3 = [2, 3, null, undefined, "array", -1, 0,{}];
+// console.log(arr3);
+
+let arr = ["hello", "good morning", "good afternoon"];
+console.log(arr);
+console.log(arr[3]);
